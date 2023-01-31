2178fec954be0097359e3a8d7b948f4cfcde06cc
Farhad Ahmed <farhadahmed2007@gmail.com>
2023-01-30 18:24:03 -0800

    first commit

---
+++ LiftBro/LiftBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftBroApp.swift
+//  LiftBro
+//
+//  Created by Farhad Ahmed on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct LiftBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
